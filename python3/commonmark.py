d02f28cd58a4083338fabdafd4f82a56392d8ea5
Felipe Morales <hel.sheep@gmail.com>
2020-05-18 16:37:07 +0200

    make the python code up to date

--- python3/commonmark.py
+++ python3/commonmark.py
@@ -1,7 +1,5 @@
 import sys
 import os.path
-import re
-from enum import Enum, auto
 from collections import namedtuple
 import pynvim
 sys.path.append(os.path.dirname(__file__))
@@ -12,30 +10,9 @@ def q(string):
     return '"' + string + '"'
 
 
-Range = namedtuple('Range', ['start', 'end'])
-
 Pos = namedtuple('Pos', ['line', 'col'])
 
 PosPair = namedtuple('PosPair', ['startpos', 'endpos'])
-
-
-class Tag(Enum):
-    Paragraph = auto()
-    Heading = auto()
-    BlockQuote = auto()
-    CodeBlock = auto()
-    List = auto()
-    Item = auto()
-    FootnoteDefinition = auto()
-    Table = auto()
-    TableHead = auto()
-    TableRow = auto()
-    TableCell = auto()
-    Emphasis = auto()
-    Strong = auto()
-    Strikethrough = auto()
-    Link = auto()
-    Image = auto()
 
 
 @pynvim.plugin
@@ -45,7 +22,6 @@ class CommonMark(object):
         self.namespace = self.vim.new_highlight_source()
         self.vim.command('hi cmarkEmphasis gui=italic')
         self.vim.command('hi cmarkStrong gui=bold')
-        self.vim.command('hi link cmarkHeading Directory')
 
     def echo(self, string):
         self.vim.command('echom ' + q(string))
@@ -90,39 +66,28 @@ class CommonMark(object):
             tail = PosPair(Pos(endpos.line, 0), Pos(endpos.line, endpos.col))
             return [head, *body, tail]
 
-    def build_hl(self, group, lnum, start_col=0, end_col=-1):
-        return [group, lnum, start_col, end_col]
-
     def highlight(self):
-        if self.vim.current.buffer.options['filetype'] != 'commonmark':
+        if self.vim.current.buffer.options['filetype'] != 'pandoc':
             return
         buf_str = "\n".join(self.vim.current.buffer)
-        offsets = libpulldowncmark.get_offsets(buf_str)
+        offsets = libvim_commonmark.get_offsets(buf_str)
         hls = []
         for i in offsets:
-            ranges, typ = offsets[i]
-
-            # internal vim byte counts start on 1,
-            # not 0 as in pulldowm-cmark
-            rng = Range(*map(lambda x: int(x) + 1, ranges.split('..')))
+            data = offsets[i]
+            typ = data['group']
 
             # canonical positions of the offsets
-            startpos = self.offset2pos(rng.start)
-            endpos = self.offset2pos(rng.end)
+            startpos = self.offset2pos(data['start'])
+            endpos = self.offset2pos(data['end'])
 
             # self.echo(self.vim.funcs.fnameescape(type))
-            if typ in [Tag.Emphasis.name, Tag.Strong.name]:
+            if typ in ('cmarkEmphasis', 'cmarkStrong'):
                 line_highlights = self.to_line_highlights(startpos, endpos)
                 for lh in line_highlights:
-                    hls.append(self.build_hl('cmark' + typ,
-                                             lh.startpos.line - 1,
-                                             lh.startpos.col,
-                                             lh.endpos.col))
-            elif re.match(Tag.Heading.name, typ):
-                line_highlights = self.to_line_highlights(startpos, endpos)
-                for lh in line_highlights:
-                    hls.append(('cmarkHeading',
-                                lh.startpos.line - 1))
+                    hls.append([typ,
+                                lh.startpos.line - 1,
+                                lh.startpos.col,
+                                lh.endpos.col])
 
         if len(hls) > 0:
             self.vim.current.buffer.update_highlights(self.namespace,
